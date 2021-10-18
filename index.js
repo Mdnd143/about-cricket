d75ebc152018059f1b912090408468f035cde294
MohdNadeem1 <90819097+Mdnd143@users.noreply.github.com>
2021-10-18 17:10:29 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,4 @@
+var readlineSync=require('readline-sync');
+var score=0;
+var usrNme=readlineSync.question('Enter Your Name  ');
+console.log("Welcome  "+usrNme+ "! here i will ask some question about nadeem. let see how well do you know nadeem?");
